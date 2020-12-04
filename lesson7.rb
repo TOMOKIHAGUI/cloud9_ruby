a09ce5cfa14e464ea51e55925da0d1c4afea4d35
higos <higosozan18@gmail.com>
2020-12-04 09:14:27 +0000

    [Finish]2-7

--- lesson7.rb
+++ lesson7.rb
@@ -1,3 +1,7 @@
-puts "キーボードから何か入力してみましょう。"
-input_key = gets
-puts "入力された内容は#{input_key}"
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+a = gets.to_i
+b = gets.to_i
+puts "計算結果を出力します。"
+puts "a *b=#{a* b}"
+puts "計算を終了します。"
